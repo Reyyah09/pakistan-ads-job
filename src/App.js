da4fc557c0b39f86779c2a048b93a7216aae1d64
Reyyah09 <arreyyahtech01@gmail.com>
2020-05-08 14:10:10 +0500

    styleguide

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-
+import './assets/style/style.scss';
 export default () => {
   return (
     <>
+   <div className="hh"> 
+     <h1 className="check">hello</h1> 
+   </div>
     </>
   );
 }
